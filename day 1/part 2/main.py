206a436538cc16d2204405010dc3210eece0ba21
Ian Pratt <ianjdpratt@gmail.com>
2021-12-01 19:01:56 -0600

    completed day 1

---
+++ day 1/part 2/main.py
@@ -0,0 +1,15 @@
+from os import pipe2
+
+
+t = 0
+p1 = 999999999999999
+p2 = 999999999999999
+p3 = 999999999999999
+for i in list(map(lambda x: int(x), open("input.txt", "r").readlines())):
+    if p2 + p1 + i > p3 + p2 + p1:
+        t += 1
+    p3 = p2
+    p2 = p1
+    p1 = i
+
+print(t)
